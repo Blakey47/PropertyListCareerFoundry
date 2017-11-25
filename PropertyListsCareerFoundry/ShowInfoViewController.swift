7d402756edf44aeab946b06dadf4e56c016934b1
Darragh Blake <darragh.blake48@gmail.com>
2017-11-25 21:30:46 +0000

    Initial Commit

---
+++ PropertyListsCareerFoundry/ShowInfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ShowInfoViewController.swift
+//  PropertyListsCareerFoundry
+//
+//  Created by Darragh on 11/25/17.
+//  Copyright © 2017 Darragh. All rights reserved.
+//
+
+import UIKit
+
+class ShowInfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
--- PropertyListsCareerFoundry/ViewController.swift
+++ PropertyListsCareerFoundry/ViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let plistUrl = Bundle.main.url(forResource: "People", withExtension: "plist")
+    let fileManager = FileManager.default
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "showInfo", sender: self)
     }
-
-
+    
 }
 
