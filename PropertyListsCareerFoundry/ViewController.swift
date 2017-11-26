93e78ac50bca8aa4f5eec246ead37f096b8755d8
Darragh Blake <darragh.blake48@gmail.com>
2017-11-26 16:21:41 +0000

    Added Functionality

--- PropertyListsCareerFoundry/ViewController.swift
+++ PropertyListsCareerFoundry/ViewController.swift
@@ -16,6 +16,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var plistFormat = PropertyListSerialization.PropertyListFormat.xml
+        
+        do {
+            let plistData = try Data(contentsOf: plistUrl!)
+            let people = try PropertyListSerialization.propertyList(from: plistData, options: [], format: &plistFormat) as! [Dictionary<String, Any>]
+            
+            for person in people {
+                print("First Name: \(String(describing: person["First Name"])), Last Name: \(String(describing: person["Last Name"])), Age: \(String(describing: person["Age"]))")
+            }
+            if let fName = people[0]["First Name"] {
+                print("First Name for first person: \(fName)")
+            }
+        } catch {
+            print("Error")
+        }
+        
     }
     
     @IBAction func showButtonPressed(_ sender: Any) {
